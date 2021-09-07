ad714fd2bee1f8e9098be9f61a7ecff8b24105fa
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2021-09-06 21:51:58 -0400

    Fix build script

--- v3/build.sh
+++ v3/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v $(pwd)/..:/root/src/v3 node:16-alpine sh -c "yarn install"
+docker run --rm -v $(pwd)/..:/root/src -w /root/src/v3 node:16-bullseye-slim bash -c "yarn install"
 docker buildx build --load -t node_ubuntu_ff .
